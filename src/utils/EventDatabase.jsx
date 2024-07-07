a6d11b9e8045ce7cdd275ddaa22aabbe9d676729
atulmaurya21 <ramkishormauryaiti@gmail.com>
2024-07-08 00:34:49 +0530

    React Project

---
+++ src/utils/EventDatabase.jsx
@@ -0,0 +1,94 @@
+export const eventList = [
+  {
+    id: 1,
+    heading: "Epic Comic Con",
+    date: { year: 2023, month: "July" },
+    location: "Mumbai, India",
+    description:
+      "Experience the most anticipated gathering of comic enthusiasts! Epic Comic Con invites you to immerse yourself in a realm where heroes and villains collide. Unveil exclusive collectibles, join panels with renowned artists, and witness legendary cosplayers bring your favorite characters to life. Whether you're a seasoned comic collector or just starting your journey, this event guarantees an epic adventure!",
+    img: "https://images.unsplash.com/photo-1612036782180-6f0b6cd846fe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29taWNzfGVufDB8fDB8fHww",
+  },
+  {
+    id: 2,
+    heading: "Fantasy Gaming Expo",
+    date: { year: 2024, month: "May" },
+    location: "Delhi, India",
+    description:
+      "Embark on a journey through fantastical realms at the Fantasy Gaming Expo! Delve into an immersive gaming experience that transcends reality. From tabletop adventures to cutting-edge VR simulations, this expo celebrates the essence of fantasy in gaming. Unleash your strategic prowess, forge alliances, and discover a world where every move shapes the destiny of kingdoms!",
+    img: "https://images.unsplash.com/photo-1511512578047-dfb367046420?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 3,
+    heading: "Cosplay Carnival",
+    date: { year: 2023, month: "September" },
+    location: "Bangalore, India",
+    description:
+      "Welcome to the Cosplay Carnival, where creativity knows no bounds! Embrace the art of transformation as cosplayers from across galaxies gather to showcase their craftsmanship. Unleash your alter ego, don your meticulously crafted costumes, and strut into a world where fantasy meets reality. With contests, workshops, and a vibrant community, this carnival invites you to step into a realm of endless possibilities!",
+    img: "https://images.unsplash.com/photo-1502136969935-8d8eef54d77b?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 4,
+    heading: "Sci-Fi Symposium",
+    date: { year: 2024, month: "March" },
+    location: "Hyderabad, India",
+    description:
+      "Dive into the future at the Sci-Fi Symposium, a convergence of cutting-edge ideas and futuristic visions! Explore the technological marvels reshaping our world, engage with visionary speakers, and ponder the possibilities of tomorrow. From AI advancements to interstellar exploration, this symposium ignites curiosity and fuels the imagination. Join us on this journey to unravel the mysteries of science fiction!",
+    img: "https://images.unsplash.com/photo-1589254065878-42c9da997008?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 5,
+    heading: "Anime Fiesta",
+    date: { year: 2023, month: "November" },
+    location: "Chennai, India",
+    description:
+      "Dive into the vibrant world of anime at Anime Fiesta! Celebrate your favorite anime series with fellow enthusiasts, participate in cosplay competitions, and discover exclusive merchandise. Join panels featuring voice actors and creators, and indulge in activities that bring your anime dreams to life. Whether you’re a die-hard fan or new to the genre, Anime Fiesta promises a fun-filled experience!",
+    img: "https://images.unsplash.com/photo-1581833971358-2c8b550f87b3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fEFuaW1lJTIwRmllc3RhfGVufDB8fDB8fHww",
+  },
+  {
+    id: 6,
+    heading: "Horror Film Festival",
+    date: { year: 2024, month: "February" },
+    location: "Kolkata, India",
+    description:
+      "Enter the realm of chills and thrills at the Horror Film Festival! Showcase your love for horror movies, meet directors and actors, and explore the art of fear. From classic horror films to the latest releases, immerse yourself in spine-tingling experiences and delve into the darkest corners of cinema. This festival is a must-visit for all horror aficionados!",
+    img: "https://images.unsplash.com/photo-1603516863860-7d5c93a83fe8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzh8fEhvcnJvciUyMEZpbG0lMjBGZXN0aXZhbHxlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    id: 7,
+    heading: "Tech Innovations Fair",
+    date: { year: 2024, month: "January" },
+    location: "Pune, India",
+    description:
+      "Discover the latest breakthroughs in technology at the Tech Innovations Fair! Engage with cutting-edge gadgets, witness live demos, and attend workshops led by industry experts. Explore advancements in AI, robotics, and more, and see how technology is transforming our lives. This fair is the perfect platform for tech enthusiasts and professionals to connect and innovate!",
+    img: "https://images.unsplash.com/photo-1491472253230-a044054ca35f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzl8fFRlY2glMjBJbm5vdmF0aW9ucyUyMEZhaXJ8ZW58MHx8MHx8fDA%3D",
+  },
+  {
+    id: 8,
+    heading: "Literary Festival",
+    date: { year: 2023, month: "December" },
+    location: "Jaipur, India",
+    description:
+      "Join us at the Literary Festival, a celebration of words and ideas! Meet acclaimed authors, attend book readings, and participate in engaging discussions. Whether you're a budding writer or an avid reader, this festival offers something for everyone. Dive into the world of literature and discover the power of storytelling at this unmissable event!",
+    img: "https://images.unsplash.com/photo-1468234847176-28606331216a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8TGl0ZXJhcnklMjBGZXN0aXZhbHxlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    id: 9,
+    heading: "Music Mania",
+    date: { year: 2024, month: "April" },
+    location: "Goa, India",
+    description:
+      "Experience the ultimate musical extravaganza at Music Mania! From rock and pop to classical and jazz, this festival brings together diverse genres and talented artists. Enjoy live performances, join music workshops, and explore interactive installations. Music Mania is a paradise for music lovers, offering a rhythmic journey that resonates with every beat!",
+    img: "https://images.unsplash.com/photo-1487180144351-b8472da7d491?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fG11c2ljJTIwbWFuaWF8ZW58MHx8MHx8fDA%3D",
+  },
+  {
+    id: 10,
+    heading: "Art and Craft Expo",
+    date: { year: 2023, month: "October" },
+    location: "Ahmedabad, India",
+    description:
+      "Immerse yourself in creativity at the Art and Craft Expo! Discover unique artworks, handmade crafts, and innovative designs. Meet artisans and creators, participate in interactive workshops, and bring home beautiful pieces of art. Whether you're an artist or an admirer, this expo offers a vibrant platform to celebrate and support the world of art and craft!",
+    img: "https://images.unsplash.com/photo-1576504365365-091376931772?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjN8fEFydCUyMGFuZCUyMENyYWZ0JTIwRXhwb3xlbnwwfHwwfHx8MA%3D%3D",
+  }
+    
+  ];
+  
